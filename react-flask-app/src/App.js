4682acbcbe4eab85cea757d871791f2c9b73f794
Shabab Ayub <s4ayub@edu.uwaterloo.ca>
2020-03-01 19:10:13 -0500

    we have routing, very simple

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -2,7 +2,97 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
+
+export default function App() {
+
+  return (
+    <div className="App">
+    <header className="App-header">
+      <Router>
+
+        nav bar??
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/patients">Patients</Link>
+          </li>
+      </ul>
+
+      <div>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/patients">
+            <Topics />
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
+    </header>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Topics() {
+  // The `path` lets us build <Route> paths that are
+  // relative to the parent route, while the `url` lets
+  // us build relative links.
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <Switch>
+        <Route exact path={path}>
+          <h2>Profiles</h2>
+          <ul>
+            <li>
+              <Link to={`${url}/patient_0`}>patient_0</Link>
+            </li>
+            <li>
+              <Link to={`${url}/patient_1`}>patient_1</Link>
+            </li>
+            <li>
+              <Link to={`${url}/patient_2`}>patient_2</Link>
+            </li>
+          </ul>
+          <h3>Please select a patient.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  // The <Route> that rendered this component has a
+  // path of `/topics/:topicId`. The `:topicId` portion
+  // of the URL indicates a placeholder that we can
+  // get from `useParams()`.
+  let { topicId } = useParams();
+
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
@@ -12,13 +102,9 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>The current time is {currentTime}.</p>
-      </header>
+    <div>
+      <h3>{topicId}</h3>
+      <h2>current time is {currentTime}</h2>
     </div>
   );
 }
-
-export default App;
